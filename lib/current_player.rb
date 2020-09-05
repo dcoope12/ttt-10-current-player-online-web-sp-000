c2752315f01aeac96e167f22758a5fbed1964278
Devin Cooper <dcoope12@asu.edu>
2020-09-05 22:42:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,14 @@ def turn_count(board)
   counter = 0
 board. each do |index|
   if index == "X" || index == "O"
-  puts counter  += 1
+  counter += 1
+end
+end
+end 
+
+def current_player(board)
+if turn_count % 2 == 0
+  return "X"
 else
-  break
-end
-end
+  return "O"
 end 
